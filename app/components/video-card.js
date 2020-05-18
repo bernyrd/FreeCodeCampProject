bb8f675411a8dfb019bb921caa609a92ff751579
AllenEsteves <estevesallen@yahoo.com>
2020-05-18 17:14:07 +0800

    First Commit

---
+++ app/components/video-card.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+
+export default class VideoCardComponent extends Component {
+    model= [{
+        title: "Ember DND Helper",
+        people: "Taras Mankovski",
+        videoLink: "peNV2yJRMLo?rel=0"
+    },{
+        title: "Dependency Injection in Angular 2",
+        people: "patrick J. Stapleton",
+        videoLink: "46WovCX8i-I?rel=0"
+    },{
+        title: "Angular CLI",
+        people: "Mike Brocchi",
+        videoLink: "BmZLpNRNnZo"
+    },{
+        title: "Angular Material 2 Spelunking & Issue Submission",
+        people: "Beh Lesh",
+        videoLink: "3gNsyL7wpXU"
+    },{
+        title: "Lorem ipsum",
+        people: "Allen Esteves",
+        videoLink: "watch?v=Q8xUvgxYjQY"
+    }];
+}
